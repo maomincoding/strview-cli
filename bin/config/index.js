df8fa9cd65da19b1c7a1d790e34afd82500488fe
maomincoding <17864296568@163.com>
2021-08-10 10:23:07 +0800

    init

---
+++ bin/config/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+	npmUrl: 'https://registry.npmjs.org/strview-cli',
+	promptTypeList: [
+		{
+			type: 'list',
+			message: 'Please select the template type to pull:',
+			name: 'type',
+			choices: [
+				{
+					name: 'fast-react-app@1.0.0',
+					value: {
+						url: 'https://github.com/maomincoding/strview-app.git',
+						gitName: 'strview-app',
+						val: 'strview-app',
+					},
+				},
+			],
+		},
+	],
+};
